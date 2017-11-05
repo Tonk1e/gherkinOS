3c90c498acb9f7dea660cefbd8338522451431dc
Tonk1e <ttt0nk1e@gmail.com>
2017-11-05 00:18:25 +0000

    made kernalmain() an external function

--- kernel.cpp
+++ kernel.cpp
@@ -1,4 +1,4 @@
-#include <stddef.h> 
+#include <stddef.h>
 #include <stdint.h> //int16_t and some integer types
 
 enum vga_color
@@ -20,19 +20,19 @@ enum vga_color
 	COLOR_LIGHT_BROWN = 14,
 	COLOR_WHITE = 15,
 };
- 
+
 uint8_t make_color(enum vga_color fg, enum vga_color bg)
 {
 	return fg | bg << 4;
 }
- 
+
 uint16_t make_vgaentry(char c, uint8_t color)
 {
 	uint16_t c16 = c;
 	uint16_t color16 = color;
 	return c16 | color16 << 8;
 }
- 
+
 size_t strlen(const char* str)
 {
 	size_t ret = 0;
@@ -40,15 +40,15 @@ size_t strlen(const char* str)
 		ret++;
 	return ret;
 }
- 
+
 static const size_t VGA_WIDTH = 80;
 static const size_t VGA_HEIGHT = 24;
- 
+
 size_t terminal_row;
 size_t terminal_column;
 uint8_t terminal_color;
 uint16_t* terminal_buffer;
- 
+
 void terminal_initialize()
 {
 	terminal_row = 0;
@@ -64,18 +64,18 @@ void terminal_initialize()
 		}
 	}
 }
- 
+
 void terminal_setcolor(uint8_t color)
 {
 	terminal_color = color;
 }
- 
+
 void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
 {
 	const size_t index = y * VGA_WIDTH + x;
 	terminal_buffer[index] = make_vgaentry(c, color);
 }
- 
+
 void terminal_putchar(char c)
 {
 	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
@@ -88,15 +88,24 @@ void terminal_putchar(char c)
 		}
 	}
 }
- 
+
 void terminal_writestring(const char* data)
 {
 	size_t datalen = strlen(data);
 	for ( size_t i = 0; i < datalen; i++ )
 		terminal_putchar(data[i]);
 }
- 
-void kernel_main()
+
+typedef void (*constructor)();
+extern "C" constructor start_ctors;
+extern "C" constructor end_ctors;
+extern "C" void callConstructors()
+{
+	for(constructor* i = &start_ctors; i != &end_ctors; i++)
+		(*i)();
+}
+
+extern "C" void kernelmain()
 {
 bool loop = true;
 terminal_initialize();
